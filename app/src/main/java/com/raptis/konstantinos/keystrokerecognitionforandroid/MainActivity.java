9bcaedeb01b062654c193092a018f2be69c565e6
konstantinos <konstantinos_raptis@hotmail.com>
2016-05-07 21:12:56 +0300

    DONE key changed to NEXT and its behaviours also changed. Key Listeners added to EditTexts

--- app/src/main/java/com/raptis/konstantinos/keystrokerecognitionforandroid/MainActivity.java
+++ app/src/main/java/com/raptis/konstantinos/keystrokerecognitionforandroid/MainActivity.java
@@ -2,8 +2,16 @@ package com.raptis.konstantinos.keystrokerecognitionforandroid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.raptis.konstantinos.keystrokerecognitionforandroid.util.Helper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +20,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.loginFormLinearLayout);
+        final EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        final EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        usernameEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(keyEvent.getKeyCode() == 87) {
+                    passwordEditText.requestFocus();
+                }
+                return false;
+            }
+        });
+
+        passwordEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(keyEvent.getKeyCode() == 87) {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(MainActivity.this.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                    passwordEditText.clearFocus();
+                    usernameEditText.clearFocus();
+                    linearLayout.requestFocus();
+                }
+                return false;
+            }
+        });
     }
 
     // sign up
